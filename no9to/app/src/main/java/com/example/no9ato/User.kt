52ff45e305ee1cbb60995f08d22c536875424804
yasofl <mineking2019@gmail.com>
2025-09-19 13:44:42 +0100

    Delete no9to directory

--- no9to/app/src/main/java/com/example/no9ato/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.no9ato
-
-data class User(var id: String = "", var name: String = "", var avatarUrl: String = "")
--- no9to/gradlew.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-:: Gradle startup script for Windows
-
-SET DIR=%~dp0
-java -jar "%DIR%\gradle\wrapper\gradle-wrapper.jar" %*
